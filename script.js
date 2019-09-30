96df7d5aa47fd8674dc184d6335c546c58eb81b0
Jakub Nakielski <you@example.com>
2019-09-30 23:42:09 +0200

    Add animations to checkboxes

---
+++ script.js
@@ -0,0 +1,9 @@
+// const element = document.getElementById("");
+
+// const fill = ev => {
+//   ev.target.parentNode.classList.toggle(
+//     (ev.target.parentNode.style.color = "black")
+//   );
+// };
+
+// document.addEventListener("click", fill);
