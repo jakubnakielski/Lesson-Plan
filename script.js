48a520b04ad937a466fedb728262179413077093
Jakub Nakielski <you@example.com>
2019-10-01 00:38:21 +0200

    Minor fix

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const button = document.getElementById("teachers__send");
+const absent = document.getElementById("absentTeachers__list");
 
+let clicked = false;
 const check = e => {
   const checkboxes = document.querySelectorAll(".teachers__checkbox");
   let checkboxesArray = new Array();
@@ -10,7 +12,14 @@ const check = e => {
     }
   });
 
-  console.log(checkboxesArray);
+  for (i = 0; i < checkboxesArray.length; i++) {
+    absent.innerHTML =
+      absent.innerHTML +
+      `<li class="absentTeachers__person">${checkboxesArray[i]}</li>`;
+  }
+  if (!checkboxesArray) {
+    button.removeEventListener("click", check); //void tab block ad editable
+  }
 };
 
 button.addEventListener("click", check);
